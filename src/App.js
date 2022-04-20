5d8b02e1cd2ed5520e682960e70b1b8326a401c7
Grayson Jones <grayson.jones@favordelivery.com>
2022-04-20 14:56:07 -0500

    [nt] setup calls

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import { useCallback, useEffect, useState } from 'react';
 
 function App() {
+
+  const initialState = {
+    businesses: [],
+    reigon: [],
+    total: 0
+  }
+
+  const searchParam = 'rice'
+  const location = {
+    latitude: null,
+    longitude: null
+  }
+ // eslint-disable-next-line no-unused-vars
+ const [restaurants, setRestaurants] = useState(initialState)
+
+  // const callYelpApi = () => (dispatch, searchParam) => {
+  //   return dispatch({
+  //     request: {
+  //       url: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${searchParam}&location=NYC`
+  //     }
+  //   })
+  // }
+
+  // this is a formatter that you could use to format the location if you got it from the browser
+  // if this does not make sense than we can go through what the code is doing
+  // eslint-disable-next-line no-unused-vars
+  function formatLocationToUrlEncode(locationObject) {
+    const objectKeys = Object.keys(locationObject)
+    objectKeys.map((key, index) => {
+        if (locationObject[key]) return `${key}=${locationObject[key]}`
+    })
+  }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect( () => {
+    const callYelpApiWithCredentials = async (searchParam, location) => {
+      const requestUrl = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search'
+     try {
+       const response = await fetch(`${requestUrl}?term=${searchParam}&location=NYC`, {
+         headers: {
+           'Authorization': `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+         }
+     })
+     const restaurantsResponse = await response.json()
+     console.log(restaurantsResponse)
+     setRestaurants(restaurantsResponse)
+
+     } catch (error) {
+       console.error(error)
+     }
+  }
+
+    callYelpApiWithCredentials(searchParam, location)
+
+  }, [])
+
   return (
     <div className="App">
-      <p>Hello World!</p>
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      </header>
+      <ul>
+      {restaurants.businesses.map(businesses => {
+          return <li>
+            {businesses.name}
+          </li>
+        })}
+      </ul>
     </div>
   );
 }
